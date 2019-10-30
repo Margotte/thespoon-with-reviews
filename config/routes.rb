ff11e2076c8daf0f6047f0a42247bcf13e22d2fd
Margotte <margo.tinawi@gmail.com>
2019-10-30 10:39:54 +0100

    Restaurant app with reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    collection do
+      get 'top'
+    end
+
+    member do
+      get 'chef'
+    end
+
+    resources :reviews, only: [:new, :create]
+  end
+
+  # get 'restaurants/top', to: 'restaurants#top'
 end
